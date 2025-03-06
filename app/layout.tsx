e03709b80531d6bc614f506d2a19f6b5801fb5cb
Tokiarivelo <anjaniaina.toky14@gmail.com>
2025-03-06 04:50:42 +0300

    [init] - Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import '@/app/ui/global.css';
+import { playwrite } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      {/* antialiased : smooth the font */}
+      <body className={`${playwrite.className} antialiased`}>{children}</body>
     </html>
   );
 }
